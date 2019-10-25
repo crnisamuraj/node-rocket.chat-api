879d3134d51670aa2836f7b380fb7a24a0f775c7
crnisamuraj <m1ivancevic@gmail.com>
2019-10-25 21:16:57 +0200

    add main and pas arguments to module

---
+++ main.js
@@ -0,0 +1,4 @@
+const postAttachment = require("./rcPostWebhook");
+
+postAttachment.post("NodeJS", "JavaScript", "We are posting this from nodeJS module. This is fuuun! :D");
+
--- rcPostWebhook.js
+++ rcPostWebhook.js
@@ -1,23 +1,28 @@
 const axios = require('axios')
 
+function post(scanPath, scanId, feedback)
+{
 
+	url = 'https://chat.doobinnovation.com/hooks/eZfAC2BgpD6vPLwS8/r9GKi89vNMJiJwh4p5PewfviPkg5pEzThNjWzBxnivJCReds'
 
-jsonObj = {
-	"text": "scanPath",
-	"attachments": [{
-		"title": "this.scanId", 
-		"text": "this.feedback", 
-		"color": "#764FF5"
-	}]
+	jsonObj = {
+		"text": scanPath,
+		"attachments": [{
+			"title": scanId, 
+			"text": feedback, 
+			"color": "#764FF5"
+		}]
+	}
+
+	axios.post(url, jsonObj)
+	.then((res) => {
+	  console.log(`statusCode: ${res.statusCode}`)
+	  console.log(res)
+	})
+	.catch((error) => {
+	  console.error(error)
+	})
+
 }
 
-url = 'https://chat.doobinnovation.com/hooks/eZfAC2BgpD6vPLwS8/r9GKi89vNMJiJwh4p5PewfviPkg5pEzThNjWzBxnivJCReds'
-
-axios.post(url, jsonObj)
-.then((res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
+exports.post = (scanPath, scanId, feedback) => post(scanPath, scanId, feedback);
